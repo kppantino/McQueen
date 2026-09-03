1664ec494db1339edb38b534b6be042a99564083
kpantino <KylaAmor.P.Pantino@smretail.com>
2026-09-03 21:50:55 +0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-8">
+      <div className="mx-auto max-w-7xl">
+        <h1 className="text-3xl font-bold text-gray-900">
+          McQueen Unified Systems
+        </h1>
+
+        <p className="mt-2 text-gray-600">
+          ROROORROROr
+        </p>
+      </div>
+    </main>
+  );
+}
